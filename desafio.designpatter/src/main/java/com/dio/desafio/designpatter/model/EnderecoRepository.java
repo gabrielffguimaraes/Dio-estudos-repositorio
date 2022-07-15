b9c17bb170136828e017565d39cd2d4d26a9bd43
unknown <gabrielffguimaraes@gmail.com>
2022-07-15 17:51:11 -0300

    desafio de projeto spring boot

---
+++ desafio.designpatter/src/main/java/com/dio/desafio/designpatter/model/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package com.dio.desafio.designpatter.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+}
