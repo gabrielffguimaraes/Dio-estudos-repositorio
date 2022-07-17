8be47613e7d2b09cd0f7f0ad59dcf03db2582930
Gabriel Ferreira Guimarães <gabrielffguimaraes@gmail.com>
2022-07-17 05:11:44 -0300

    updates

--- desafio.designpatter/src/main/java/com/dio/desafio/designpatter/model/ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dio.desafio.designpatter.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- desafio.designpatter/src/main/java/com/dio/desafio/designpatter/model/EnderecoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dio.desafio.designpatter.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, String> {
-}
